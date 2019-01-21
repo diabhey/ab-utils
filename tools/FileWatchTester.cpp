4daa2dfdea0fa1212bc6291c5dacf33d92d23f8b
¨bigillu¨ <abhimanyuselvan@protonmail.com>
2019-01-21 11:54:39 +0100

    Implemented basic file monitoring functionality

---
+++ tools/FileWatchTester.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file FileWatchTester.cpp
+ * @author bigillu
+ * @brief File Watcher tester application
+ * Code compilation
+ *   g++ -std=c++17 -Wall -pedantic FileWatchTester.cpp -lstdc++fs
+ *   clang++ -std=c++17 -stdlib=libc++ -Wall -pedantic FileWatchTester.cpp -lc++fs
+ * @version 0.1
+ * @date 2019-01-21
+ *
+ * @copyright Copyright (c) 2019
+ */
+
+#include <iostream>
+#include "FileWatcher.h"
+
+int main() {
+  // Create a FileWatcher instance that will check the current folder for
+  // changes every 5 seconds
+  FileWatcher fw{"/home/bigillu/code/libs/utilities/tools",
+                 std::chrono::milliseconds(5000)};
+
+  // Start monitoring a folder for changes and (in case of changes)
+  // run a user provided lambda function
+  fw.start([](std::string pathToWatch, FileStatus status) -> void {
+    // Process only regular files, all other file types are ignored
+    if (!std::experimental::filesystem::is_regular_file(
+            std::experimental::filesystem::path(pathToWatch)) &&
+        status != FileStatus::erased) {
+      return;
+    }
+
+    switch (status) {
+      case FileStatus::created:
+        std::cout << "File created: " << pathToWatch << '\n';
+        break;
+      case FileStatus::modified:
+        std::cout << "File modified: " << pathToWatch << '\n';
+        break;
+      case FileStatus::erased:
+        std::cout << "File erased: " << pathToWatch << '\n';
+        break;
+      default:
+        std::cout << "Error! Unknown file status.\n";
+    }
+  });
+}
--- tools/FileWatcher.h
+++ tools/FileWatcher.h
@@ -13,8 +13,12 @@
 
 #include <chrono>
 #include <experimental/filesystem>
+#include <functional>
 #include <string>
+#include <thread>
 #include <unordered_map>
+
+enum class FileStatus { created, modified, erased };
 
 class FileWatcher {
  public:
@@ -25,12 +29,46 @@ class FileWatcher {
          std::experimental::filesystem::recursive_directory_iterator(
              mPathToWatch)) {
       mFilePaths[file.path()] =
-          std::experimental::filesystem::last_write_time(file.path());
+          std::experimental::filesystem::last_write_time(file);
+    }
+  }
+
+  void start(const std::function<void(std::string, FileStatus)>& exec) {
+    while (mRunning) {
+      // Wait for the delay
+      std::this_thread::sleep_for(mDelay);
+      // Check to see if an existing file has been erased.
+      for (auto& file : mFilePaths) {
+        if (!std::experimental::filesystem::exists(file.first)) {
+          exec(file.first, FileStatus::erased);
+          mFilePaths.erase(file.first);
+        }
+      }
+      // Check to see if a file has been created or modified.
+      for (auto& file :
+           std::experimental::filesystem::recursive_directory_iterator(
+               mPathToWatch)) {
+        auto file_latest_write_time =
+            std::experimental::filesystem::last_write_time(file);
+
+        if (not(mFilePaths.find(file.path()) != mFilePaths.end())) {
+          mFilePaths[file.path()] = file_latest_write_time;
+          exec(file.path(), FileStatus::created);
+        }
+        // File modification check
+        else {
+          if (mFilePaths[file.path()] != file_latest_write_time) {
+            mFilePaths[file.path()] = file_latest_write_time;
+            exec(file.path(), FileStatus::modified);
+          }
+        }
+      }
     }
   }
 
  private:
   std::string mPathToWatch;
+  bool mRunning = true;
   std::chrono::duration<unsigned int, std::milli> mDelay;
   std::unordered_map<std::string, std::experimental::filesystem::file_time_type>
       mFilePaths;
