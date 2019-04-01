90e46f6774c95f3f68f1f8415b87dd2d0102f28c
¨bigillu¨ <abhimanyuselvan@protonmail.com>
2019-04-01 23:56:44 +0200

    Initial benchmarking

--- tools/pgnlib/src/PgnLib.cpp
+++ tools/pgnlib/src/PgnLib.cpp
@@ -11,10 +11,24 @@
 
 /*! Headers */
 #include <benchmark/benchmark.h>
+#include <algorithm>
 #include <fstream>
 #include "PgnDisplay.h"
 #include "PgnExtractor.h"
 #include "PgnValidator.h"
+
+using namespace benchmark;
+
+static void BM_PgnLib(benchmark::State& state) {
+  while (state.KeepRunning()) {
+    std::ifstream istr("SingleGame.pgn");
+    PgnLib::PgnExtractor extract(istr);
+    PgnLib::PgnValidator validate(extract.GetMoveMaps());
+    PgnLib::PgnDisplay display(validate.GetValidatedMoveMaps());
+  }
+}
+
+BENCHMARK(BM_PgnLib)->Arg(1000);
 
 int main(int argc, char* argv[]) {
   try {
@@ -22,13 +36,10 @@ int main(int argc, char* argv[]) {
       std::cerr << "Usage: ./PgnLib <pgn file path>" << std::endl;
       return EXIT_FAILURE;
     }
-
-    std::ifstream istr(argv[1]);
-    PgnLib::PgnExtractor extract(istr);
-    PgnLib::PgnValidator validate(extract.GetMoveMaps());
-    PgnLib::PgnDisplay display(validate.GetValidatedMoveMaps());
-
-  } catch (std::exception& e) {
+    benchmark::Initialize(&argc, argv);
+    benchmark::RunSpecifiedBenchmarks();
+  }
+  catch (std::exception& e) {
     std::cerr << "Exception: " << e.what() << std::endl;
   }
   return EXIT_SUCCESS;
