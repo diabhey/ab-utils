d3a3f3b8e8a8c19da8c5345d53dfd717b085d9ea
¨bigillu¨ <abhimanyuselvan@protonmail.com>
2019-07-20 22:54:45 +0200

    Minor changes

--- snippets/async/boost-asio-playground/src/main.cpp
+++ snippets/async/boost-asio-playground/src/main.cpp
@@ -12,26 +12,29 @@
 #include <boost/thread.hpp>
 #include <iostream>
 
-boost::asio::io_service io_service;
 
-void WorkerThread() {
+void WorkerThread(boost::shared_ptr<boost::asio::io_service> io_service) {
   std::cout << "Start Thread\n";
-  io_service.run();
+  io_service->run();
   std::cout << "Finish Thread\n";
 }
 
 int main(int argc, char* argv[]) {
+
+  boost::shared_ptr<boost::asio::io_service> io_service(
+      boost::make_shared<boost::asio::io_service>());
+
   boost::shared_ptr<boost::asio::io_service::work> work(
-      boost::make_shared<boost::asio::io_service::work>(io_service));
+      boost::make_shared<boost::asio::io_service::work>(*io_service));
 
   std::cout << "Press [return] to exit.\n";
-  std::vector<std::thread> worker_threads;
+  std::vector<boost::thread> worker_threads;
   for (int x = 0; x < 4; ++x) {
-    worker_threads.emplace_back(boost::bind(WorkerThread));
+    worker_threads.emplace_back(boost::bind(WorkerThread, io_service));
   }
 
   std::cin.get();     // Wait to receive the exit input from the user
-  io_service.stop();  // Stopping all the existing running services
+  io_service->stop();  // Stopping all the existing running services
 
   // Joining all the threads
   for (auto& thread : worker_threads) {
